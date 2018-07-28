b294d055085dce355c67c806b61e6e893a4bbafa
schmitzkr <kyle.schmitz@gmail.com>
2018-07-27 17:28:05 -0700

    updated to use join and slice list

--- cleaner.py
+++ cleaner.py
@@ -4,9 +4,15 @@ lines = f.readlines()
 f.close()
 
 g = open('dirt.txt', 'w')
+list = []
 for item in lines:
 	s = str(item)
 	i = s.index(" ")
-	g.write(item[:i] + ", ")
+	s = item[:i]
+	list.append(s)
+
+a = str(", ".join(list))
+g.write(a[i:i+19] for i in xrange(0,len(a),19))
+
 g.close()
-	
+#print(list)
